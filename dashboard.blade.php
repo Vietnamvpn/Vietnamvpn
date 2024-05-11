870414b9a001c814a4a15cebda52f9bc20897ca0
Vietnamvpn <kangst24h@gmail.com>
2024-05-11 23:30:32 +0800

    Add files via upload

---
+++ dashboard.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="/theme/{{$theme}}/assets/components.chunk.css?v={{$version}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="/theme/{{$theme}}/assets/umi.css?v={{$version}}">
+        <meta charset="utf-8">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://Vietnamvpn.pro.vn/image/logo6.jpg">
+    <link rel="icon" href='https://Vietnamvpn.pro.vn/image/logo6.jpg'>
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no">
+        <meta name="theme-color" content="#0665d0">
+
+    <title>Vietnamvpn.pro.vn</title>
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700"> -->
+    <script>window.routerBase = "/";</script>
+    <script>
+        window.settings = {
+            title: 'Vietnamvpn.pro.vn',
+            assets_path: '/theme/{{$theme}}/assets',
+            theme: {
+                sidebar: '{{$theme_config['theme_sidebar']}}',
+                header: '{{$theme_config['theme_header']}}',
+                color: '{{$theme_config['theme_color']}}',
+            },
+            version: '{{$version}}',
+            background_url: 'https://pixabay.com/vi/gifs/d%E1%BA%A5u-sao-chi%C3%AAm-tinh-h%E1%BB%8Dc-l%C3%A1-s%E1%BB%91-t%E1%BB%AD-vi-3225/',
+            description: '{{$description}}',
+            i18n: [
+                'zh-CN',
+                'en-US',
+                'ja-JP',
+                'vi-VN',
+                'ko-KR',
+                'zh-TW',
+                'fa-IR'
+            ],
+            logo: '{{$logo}}'
+        }
+    </script>
+    <script src="/theme/{{$theme}}/assets/i18n/zh-CN.js?v={{$version}}"></script>
+    <script src="/theme/{{$theme}}/assets/i18n/zh-TW.js?v={{$version}}"></script>
+    <script src="/theme/{{$theme}}/assets/i18n/en-US.js?v={{$version}}"></script>
+    <script src="/theme/{{$theme}}/assets/i18n/ja-JP.js?v={{$version}}"></script>
+    <script src="/theme/{{$theme}}/assets/i18n/vi-VN.js?v={{$version}}"></script>
+    <script src="/theme/{{$theme}}/assets/i18n/ko-KR.js?v={{$version}}"></script>
+    <script src="/theme/{{$theme}}/assets/i18n/fa-IR.js?v={{$version}}"></script>
+</head>
+
+<body>
+<div id="root"></div>
+<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="54f47b9c-ac43-4958-b6ca-0437bd1d2015";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+
+
+//giphy
+
+ <style>
+        #toggleButton {
+            position: fixed;
+            bottom: -20px;
+            left: -180px;
+            background-color: transparent;
+            border: none;
+            cursor: pointer;
+            font-size: 20px;
+            color: #333;
+            z-index: 9999;
+        }
+
+      
+
+#toggleButton img {
+    width: 25%;
+    height: auto;
+}
+    </style>
+   <button id="toggleButton" class="blink"><img src="https://media3.giphy.com/media/n881nu8KO2tJ5VtJ7U/giphy.webp?cid=790b7611xbfd5yp7qslg8phq7dkv2dt1faevmlnraqn3z1r2&ep=v1_stickers_search&rid=giphy.webp&ct=s" alt="Toggle Audio"></button>
+<script src="/theme/{{$theme}}/assets/vendors.async.js?v={{$version}}"></script>
+<script src="/theme/{{$theme}}/assets/components.async.js?v={{$version}}"></script>
+<script src="/theme/{{$theme}}/assets/umi.js?v={{$version}}"></script>
+</body>
+
+</html>
